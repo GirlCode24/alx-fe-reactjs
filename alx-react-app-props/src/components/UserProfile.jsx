56293115228ec27842559cfc1175a9c7c5b4c662
GirlCode24 <janetquayenaa@gmail.com>
2025-09-04 15:39:07 +0000

    usercontext bug fix

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,15 @@
+import React, { useContext } from "react";
+import { UserContext } from "../components/UserContext"; 
 
-function UserProfile(props) {
+function UserProfile() {
+  const { user } = useContext(UserContext);
+
   return (
     <div>
       <h1>User Profile</h1>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
+      <h2>{user.name}</h2>
+      <p>Age: {user.age}</p>
+      <p>Bio: {user.bio}</p>
     </div>
   );
 }
